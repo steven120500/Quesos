fc0c7e0353812d0b3a8095501f6efb6f34bcdd2f
Steven Corrales <scorrales18@gmail.com>
2026-09-11 14:48:56 -0600

    Final

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ujehmcitkgdspowmygaa.supabase.co';
-const supabaseKey = 'pega_aqui_tu_clave_larga_que_empieza_por_eyJ';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVqZWhtY2l0a2dkc3Bvd215Z2FhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNTI4ODgsImV4cCI6MjEwNDcyODg4OH0.mL66H6aBq_e79P5RMoxRFL2VaKeQeZ4BJy0Kaine9Do';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
