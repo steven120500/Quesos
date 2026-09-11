b80f6d4384b30aca2925bb346f3a6862036f3e52
Steven Corrales <scorrales18@gmail.com>
2026-09-11 14:38:16 -0600

    Estable

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,10 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY || import.meta.env.VITE_SUPABASE_ANON_KEY;
-
-if (!supabaseUrl || !supabaseKey) {
-  console.error("⚠️ ALERTA: Faltan las variables en tu archivo .env");
-}
+const supabaseUrl = 'https://ujehmcitkgdspowmygaa.supabase.co';
+const supabaseKey = 'pega_aqui_tu_clave_larga_que_empieza_por_eyJ';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
